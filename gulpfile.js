5140c2d0d2f6121c566f7515ff3a98f8045e9bc0
Lucas Gregory <124362765+oLucasGregory@users.noreply.github.com>
2024-06-04 12:56:56 +0100

    exame

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens(){
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+
+}
+
+function comprimeJavaScript(){
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'))
+}
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/styles'));
+}
+
+
+
+exports.default = function(){
+    gulp.watch('./source/style/*.scss',{ignoreInitial: false}, gulp.series(compilaSass));
+    gulp.watch('./source/script/*.scss',{ignoreInitial: false}, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/images/*',{ignoreInitial: false}, gulp.series(comprimeImagens));
+}
