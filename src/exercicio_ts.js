1a62d602c816b27bd48dd03339d9b049aacca9ee
Lucas Gregory <124362765+oLucasGregory@users.noreply.github.com>
2024-08-21 14:45:30 +0100

    exercicio ts

---
+++ src/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function calcularNumeros(base, altura) {
+    return base * altura;
+}
+console.log(calcularNumeros(10, 50));
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+console.log(dizOla("Lucas"));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,12 @@
+function calcularNumeros(base: number, altura: number){
+    return base * altura
+     
+}
+console.log(calcularNumeros(10,50))
+
+
+function dizOla(nome: string){
+   return `Olá ${nome}`
+}
+
+console.log(dizOla("Lucas"))
