bd937b8b7c4c2540346132270da06792b4c2233c
Lucas Gregory <124362765+oLucasGregory@users.noreply.github.com>
2024-08-22 12:09:13 +0100

    tipando resultados

--- src/exercicio_ts.ts
+++ src/exercicio_ts.ts
@@ -1,11 +1,11 @@
-function calcularNumeros(base: number, altura: number){
+function calcularNumeros(base: number, altura: number): number{
     return base * altura
      
 }
 console.log(calcularNumeros(10,50))
 
 
-function dizOla(nome: string){
+function dizOla(nome: string): string{
    return `Olá ${nome}`
 }
 
